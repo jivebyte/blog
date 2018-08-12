227e5342c09b236e8f3629bae627cdc7f3c6d54c
Jone Tabaiwalu <jn10t5@yahoo.com>
2018-08-12 19:10:31 +1200

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About: Info</title>
+</head>
+<body>
+    <h1>About Us</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita minima earum, quod nihil, ad culpa illo nisi adipisci alias ea dolorum corrupti aliquam tenetur laboriosam exercitationem perferendis, vel aspernatur laborum?</p>
+</body>
+</html>
